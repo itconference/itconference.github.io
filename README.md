f6317bc293d472f051c8f8e4ed624c92321f59fe
John Doe <john@doe.org>
2018-06-19 15:59:22 +0200

    the beginning

---
+++ README.md
@@ -0,0 +1,25 @@
+# itconference.top
+
+> Hi! I am itconference.top. I am making picking the right tech conference easy.
+
+This web app comprises of `Vue` client app.
+
+## How to run the app locally?
+
+``` bash
+# make sure yarn is installed
+npm install --global yarn
+
+# install client app dependencies
+yarn
+
+# run client app
+yarn run dev
+```
+
+Voila! The app is available via port `8080`.
+
+## Build for production
+
+...in progress...
+
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+/* eslint-disable import/no-commonjs */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const WriteFilePlugin = require('write-file-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+
+const CnameWebpackPlugin = require('cname-webpack-plugin');
+const webpack = require('webpack');
+
+const dest = path.resolve(__dirname, './dist');
+
+const conferences = yaml.load(fs.readFileSync(path.resolve(__dirname, 'conferences.yaml'), {encoding: 'utf-8'}))
+const year = { year: new Date().getFullYear() } 
+const locals = Object.assign({}, conferences, year)
+
+module.exports = {
+  output: {
+    path: dest,
+    publicPath: '/',
+    filename: `[name].[hash].js`
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.pug'}),
+    new CopyWebpackPlugin([
+      { from: "src/static" }
+    ]),
+    new WriteFilePlugin()
+  ],  
+  module: {
+    rules: [
+      {
+        loader: 'source-loader'
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-static-loader',
+        options: {
+          pretty: false,
+          locals: locals,
+        }
+      },    
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+        }
+      }
+    ],
+  },
+  devServer: {
+    historyApiFallback: false
+  },
+  performance: {
+    hints: false,
+  }
+};
